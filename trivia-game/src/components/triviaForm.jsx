cd90cc641e63e5d8bb528de1e40903b00d728312
DCamron <dcamrondunlap@gmail.com>
2024-04-09 08:40:15 -0400

    changed from form submit to see if that works

--- trivia-game/src/components/triviaForm.jsx
+++ trivia-game/src/components/triviaForm.jsx
@@ -12,8 +12,7 @@ function TriviaForm() {
   const [type, setType] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleStartQuiz = async () => {
     try {
       const response = await axios.get('http://localhost:9002/api/trivia/questions', {
         params: {
@@ -25,9 +24,8 @@ function TriviaForm() {
       });
       console.log('Fetched trivia questions:', response.data);
       navigate('/quiz', { state: { quizData: response.data } });
-      console.log('Navigated to questions page with quiz data:', response.data);
     } catch (error) {
-      console.error('Error fetching trivia questions', error);
+      console.error('Error fetching trivia questions:', error);
     }
   };
 
@@ -35,7 +33,7 @@ function TriviaForm() {
     <div className='absolute inset-0 flex justify-center items-center bg-[#071330]'>
       <div className="p-6 bg-[#0C4160] rounded-xl max-w-md">
         <h2 className="text-xl font-semibold mb-4 text-center text-[#C3CEDA]">Create your Game!</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-4">
           <label className="block">
             <span className="block text-lg text-center text-[#C3CEDA]">Number of Questions</span>
             <input
@@ -106,20 +104,22 @@ function TriviaForm() {
               <option disabled value=''>Choose a type</option>
               <option value="multiple">Multiple Choice</option>
               <option value="boolean">True/False</option>
+              
             </select>
           </label>
 
-          <button type="submit"
-          className="btn bg-[#738FA7] block mx-auto px-6 rounded-md"
-          onClick={handleSubmit}
+          <button
+            className="btn bg-[#738FA7] block mx-auto px-6 rounded-md"
+            onClick={handleStartQuiz}
           >
             Start!
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
 }
+
 
 
 export default TriviaForm

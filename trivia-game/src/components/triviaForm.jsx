af91d9740f6bc199f0be24c6598993fd44814298
DCamron <dcamrondunlap@gmail.com>
2024-04-09 09:26:43 -0400

    trying to add the button as label to see if that works

--- trivia-game/src/components/triviaForm.jsx
+++ trivia-game/src/components/triviaForm.jsx
@@ -34,86 +34,50 @@ function TriviaForm() {
       <div className="p-6 bg-[#0C4160] rounded-xl max-w-md">
         <h2 className="text-xl font-semibold mb-4 text-center text-[#C3CEDA]">Create your Game!</h2>
         <div className="space-y-4">
-          <label className="block">
-            <span className="block text-lg text-center text-[#C3CEDA]">Number of Questions</span>
-            <input
-              type="number"
-              value={numberOfQuestions}
-              onChange={(e) => setNumberOfQuestions(e.target.value)}
-              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-              max={50}
-            />
+          <label className="block" onClick={handleStartQuiz} style={{ cursor: 'pointer' }}>
+            <span className="block text-lg text-center text-[#C3CEDA]">Start!</span>
           </label>
-
-          <label className="block">
-            <span className="block text-lg text-center text-[#C3CEDA]">Category</span>
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-            >
-              <option disabled value=''>Choose a category</option>
-              <option value="9">General Knowledge</option>
-              <option value="10">Entertainment: Books</option>
-              <option value="11">Entertainment: Film</option>
-              <option value="12">Entertainment: Music</option>
-              <option value="13">Entertainment: Musicals & Theatres</option>
-              <option value="14">Entertainment: Television</option>
-              <option value="15">Entertainment: Video Games</option>
-              <option value="16">Entertainment: Board Games</option>
-              <option value="17">Science & Nature</option>
-              <option value="18">Science: Computers</option>
-              <option value="19">Science: Mathematics</option>
-              <option value="20">Mythology</option>
-              <option value="21">Sports</option>
-              <option value="22">Geography</option>
-              <option value="23">History</option>
-              <option value="24">Politics</option>
-              <option value="25">Art</option>
-              <option value="26">Celebrities</option>
-              <option value="27">Animals</option>
-              <option value="28">Vehicles</option>
-              <option value="29">Entertainment: Comics</option>
-              <option value="30">Science: Gadgets</option>
-              <option value="31">Entertainment: Japanese Anime & Manga</option>
-              <option value="32">Entertainment: Cartoon & Animations</option>
-            </select>
-          </label>
-
-          <label className="block">
-            <span className="block text-lg text-center text-[#C3CEDA]">Difficulty</span>
-            <select
-              value={difficulty}
-              onChange={(e) => setDifficulty(e.target.value)}
-              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-            >
-              <option disabled value=''>Choose a difficulty</option>
-              <option value="easy">Easy</option>
-              <option value="medium">Medium</option>
-              <option value="hard">Hard</option>
-            </select>
-          </label>
-
-          <label className="block">
-            <span className="block text-lg text-center text-[#C3CEDA]">Type</span>
-            <select
-              value={type}
-              onChange={(e) => setType(e.target.value)}
-              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-            >
-              <option disabled value=''>Choose a type</option>
-              <option value="multiple">Multiple Choice</option>
-              <option value="boolean">True/False</option>
-              
-            </select>
-          </label>
-
-          <button
-            className="btn bg-[#738FA7] block mx-auto px-6 rounded-md"
-            onClick={handleStartQuiz}
+          <input
+            type="number"
+            value={numberOfQuestions}
+            onChange={(e) => setNumberOfQuestions(e.target.value)}
+            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+            max={50}
+            placeholder="Number of Questions"
+          />
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
           >
-            Start!
-          </button>
+            <option disabled value=''>Choose a category</option>
+            <option value="9">General Knowledge</option>
+            <option value="10">Entertainment: Books</option>
+            <option value="11">Entertainment: Film</option>
+            <option value="12">Entertainment: Music</option>
+            {/* Include other category options */}
+          </select>
+          <select
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+          >
+            <option disabled value=''>Choose a difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+            {/* Include other difficulty options */}
+          </select>
+          <select
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+          >
+            <option disabled value=''>Choose a type</option>
+            <option value="multiple">Multiple Choice</option>
+            <option value="boolean">True/False</option>
+            {/* Include other type options */}
+          </select>
         </div>
       </div>
     </div>

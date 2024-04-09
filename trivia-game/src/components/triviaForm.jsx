5e7ce6d6b07976f980b13384f29ffc2c7d04b269
DCamron <dcamrondunlap@gmail.com>
2024-04-09 10:02:32 -0400

    add onTouchStart to see if that helps

--- trivia-game/src/components/triviaForm.jsx
+++ trivia-game/src/components/triviaForm.jsx
@@ -33,52 +33,67 @@ function TriviaForm() {
     <div className='absolute inset-0 flex justify-center items-center bg-[#071330]'>
       <div className="p-6 bg-[#0C4160] rounded-xl max-w-md">
         <h2 className="text-xl font-semibold mb-4 text-center text-[#C3CEDA]">Create your Game!</h2>
-        <div className="space-y-4">
-          <label className="block" onClick={handleStartQuiz}>
-            <span className="block text-lg text-center text-[#C3CEDA] ">Start!</span>
+        <form onSubmit={handleStartQuiz} className="space-y-4">
+          <label className="block">
+            <span className="block text-lg text-center text-[#C3CEDA]">Number of Questions</span>
+            <input
+              type="number"
+              value={numberOfQuestions}
+              onChange={(e) => setNumberOfQuestions(e.target.value)}
+              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+              max={50}
+            />
           </label>
-          <input
-            type="number"
-            value={numberOfQuestions}
-            onChange={(e) => setNumberOfQuestions(e.target.value)}
-            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-            max={50}
-            placeholder="Number of Questions"
-          />
-          <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+
+          <label className="block">
+            <span className="block text-lg text-center text-[#C3CEDA]">Category</span>
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+            >
+              <option value="">Select a category</option>
+              <option value="9">General Knowledge</option>
+              {/* Add more category options here */}
+            </select>
+          </label>
+
+          <label className="block">
+            <span className="block text-lg text-center text-[#C3CEDA]">Difficulty</span>
+            <select
+              value={difficulty}
+              onChange={(e) => setDifficulty(e.target.value)}
+              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+            >
+              <option value="">Select a difficulty</option>
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
+          </label>
+
+          <label className="block">
+            <span className="block text-lg text-center text-[#C3CEDA]">Type</span>
+            <select
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+              className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
+            >
+              <option value="">Select a type</option>
+              <option value="multiple">Multiple Choice</option>
+              <option value="boolean">True/False</option>
+            </select>
+          </label>
+
+          <button
+            type="button"
+            className="btn bg-[#738FA7] block mx-auto px-6 rounded-md"
+            onClick={handleStartQuiz}
+            onTouchStart={handleStartQuiz} // Add touch event listener
           >
-            <option disabled value=''>Choose a category</option>
-            <option value="9">General Knowledge</option>
-            <option value="10">Entertainment: Books</option>
-            <option value="11">Entertainment: Film</option>
-            <option value="12">Entertainment: Music</option>
-            {/* Include other category options */}
-          </select>
-          <select
-            value={difficulty}
-            onChange={(e) => setDifficulty(e.target.value)}
-            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-          >
-            <option disabled value=''>Choose a difficulty</option>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-            {/* Include other difficulty options */}
-          </select>
-          <select
-            value={type}
-            onChange={(e) => setType(e.target.value)}
-            className="block w-full border bg-[#738FA7] rounded-md px-4 py-2"
-          >
-            <option disabled value=''>Choose a type</option>
-            <option value="multiple">Multiple Choice</option>
-            <option value="boolean">True/False</option>
-            {/* Include other type options */}
-          </select>
-        </div>
+            Start!
+          </button>
+        </form>
       </div>
     </div>
   );

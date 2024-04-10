0efe4db465eec65d3b96ef9730c9d1c906f050f8
DCamron <dcamrondunlap@gmail.com>
2024-04-10 13:25:37 -0400

    testing for netlify

---
+++ trivia-game/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
+const app = express();
+const PORT = process.env.PORT || 9002;
+
+app.use(cors())
+
+app.get('/api/trivia/questions', async (req, res) => {
+  try {
+    const {amount = 10, category, difficulty, type} = req.query
+    let apiURL = `https://opentdb.com/api.php?amount=${amount}`;
+
+    if (category) apiURL += `&category=${category}`;
+    if (difficulty) apiURL += `&difficulty=${difficulty}`;
+    if (type) apiURL += `&type=${type}`;
+
+    const response = await axios.get(apiURL);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching trivia questions', error);
+    res.status(500).json({error: 'An error occurred while fetching questions'});
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
